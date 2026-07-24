f12fb33288875d5f9f62eed5165ef926aa7e8748
changvito <85725501+changvito@users.noreply.github.com>
2026-07-24 14:10:24 +0000

    Contains Duplicate (cpp)

---
+++ 217-contains-duplicate/solution.cpp
@@ -0,0 +1,13 @@
+﻿class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        std:: unordered_map <int, int> HashMap;
+        for ( int i = 0; i < nums.size(); i++ ){
+            if ( HashMap.find(nums[i]) != HashMap.end()){
+            return true;
+            }
+            HashMap[nums[i]] = i;
+        }
+        return false;
+    }
+};
