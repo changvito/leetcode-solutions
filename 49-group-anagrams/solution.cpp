40d9334ff910283fc6810a6a4717a603a579099c
changvito <85725501+changvito@users.noreply.github.com>
2026-07-25 02:28:27 +0000

    Group Anagrams (cpp)

---
+++ 49-group-anagrams/solution.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map <string, vector<string>> HashMap;
+        for (auto i : strs){
+            string original = i;
+            std:: sort(i.begin(), i.end());
+            HashMap[i].push_back(original);
+        }
+        vector<vector<string>> ans;
+        for (auto i : HashMap)
+            ans.push_back(i.second);
+        return ans;
+
+    }
+};
