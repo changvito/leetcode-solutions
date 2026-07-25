4b2795a278f571c5d3ae5a2a09bb9f5e853bcbb0
changvito <85725501+changvito@users.noreply.github.com>
2026-07-25 01:27:23 +0000

    Valid Anagram (cpp)

---
+++ 242-valid-anagram/solution.cpp
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        std::unordered_map<char,int> HashMap;
+        for (int i = 0; i < s.size(); i ++){
+            HashMap[s[i]]++;
+        }
+        for (int i = 0; i < t.size(); i ++){
+            HashMap[t[i]]--;
+        }
+        if (s.size() != t.size()){
+            return false;
+        }
+        else{
+            for (int i = 0; i < s.size(); i++){
+                if (HashMap[s[i]] != 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
