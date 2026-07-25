9ae8d836e6b1d90e148c7b6406dc938e7c794bb3
changvito <85725501+changvito@users.noreply.github.com>
2026-07-25 06:26:28 +0000

    Top K Frequent Elements (cpp)

---
+++ 347-top-k-frequent-elements/solution.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        std:: unordered_map <int, int> HashMap;
+        for (const auto& x : nums){
+            HashMap[x]++;
+        }
+        std:: vector<pair<int, int>> HashVec(HashMap.begin(), HashMap.end());
+        std:: sort(HashVec.begin(),HashVec.end(), [](const auto& a, const auto& b){
+            return a.second > b.second;
+        });
+        vector<int> ans;
+        for (int i = 0; i < k; i++){
+            ans.push_back(HashVec[i].first);
+        }
+        return ans;
+    }
+};
