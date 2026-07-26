3a782764957f1fc664b4bd52d515dee22c96cfe2
changvito <85725501+changvito@users.noreply.github.com>
2026-07-26 03:39:35 +0000

    Longest Consecutive Sequence (cpp)

---
+++ 128-longest-consecutive-sequence/solution.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        std:: unordered_map<int, int> island;
+        int MaxLength = 0;
+        for (int i : nums){
+            if (!island[i]){
+                island[i] = island[i-1] + island[i+1] + 1;
+                island[i+island[i+1]] = island[i];
+                island[i-island[i-1]] = island[i];
+                MaxLength = max(MaxLength, island[i]);
+            }
+        }
+        return MaxLength;
+    }
+};
