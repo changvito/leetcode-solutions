31e846d61e0a6b2677bc286360860242a98d3fda
changvito <85725501+changvito@users.noreply.github.com>
2026-07-27 13:28:32 +0000

    Longest Consecutive Sequence (cpp)

--- 128-longest-consecutive-sequence/solution.cpp
+++ 128-longest-consecutive-sequence/solution.cpp
@@ -1,19 +1,24 @@
 ﻿class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        std:: unordered_set<int> NumSet(nums.begin(), nums.end());
-        int LongestConsecutiveSequence = 0;
-        for (int i : NumSet){
-            if (NumSet.find(i - 1) ==  NumSet.end()){
-                int CurrentNum = i;
-                int CurrentSequence = 1;
-                while (NumSet.find(CurrentNum + 1) != NumSet.end()){
-                    CurrentNum += 1;
-                    CurrentSequence += 1;
-                }
-                LongestConsecutiveSequence = max(LongestConsecutiveSequence, CurrentSequence);
-            }
+        if (nums.empty()){
+            return 0;
         }
-        return LongestConsecutiveSequence;
+        std:: sort(nums.begin(), nums.end());
+        int Current = 1;
+        int Max = 1;
+        for (int i = 1; i < nums.size(); i++){
+            if (nums[i] == nums[i-1]){
+                continue;
+            }
+            if (nums[i] == nums[i-1] + 1){
+                Current ++;
+            }
+            else{
+                Current = 1;
+            }
+            Max = max(Max, Current);
+        }
+        return Max;
     }
 };
