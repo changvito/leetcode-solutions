d9fff8da675767efef01a1b64d82fa1ac975e885
changvito <85725501+changvito@users.noreply.github.com>
2026-07-29 12:13:10 +0000

    Valid Palindrome (cpp)

---
+++ 125-valid-palindrome/solution.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+    bool isPalindrome(string s) {
+        std:: string LowerCaseString;
+        for (char c : s){
+            if (isalnum(c)){
+                LowerCaseString += tolower(c);
+            }
+        }
+        for (int i = 0; i < LowerCaseString.size()/2; i++){
+            if (LowerCaseString[i] != LowerCaseString[LowerCaseString.size() - i - 1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
