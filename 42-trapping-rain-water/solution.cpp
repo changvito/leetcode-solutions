1280ac3166e7d3e2ab5eaed76ed57ac91cd78567
changvito <85725501+changvito@users.noreply.github.com>
2026-08-01 08:43:39 +0000

    Trapping Rain Water (cpp)

---
+++ 42-trapping-rain-water/solution.cpp
@@ -0,0 +1,31 @@
+﻿class Solution {
+public:
+    int trap(vector<int>& height) {
+        int Left = 0;
+        int Right = height.size() - 1;
+        int LeftMax = 0;
+        int RightMax = 0;
+        int Water = 0;
+        while (Left < Right){
+            if (height[Left] < height[Right]){
+                if (height[Left] >= LeftMax){
+                    LeftMax = height[Left];
+                }
+                else{
+                    Water += LeftMax - height[Left];
+                }
+                Left++;
+            }
+            else {
+                if (height[Right] >= RightMax){
+                    RightMax = height[Right];
+                }
+                else{
+                    Water += RightMax - height[Right]; 
+                }
+                Right--;
+            }
+        }
+        return Water;
+    }
+};
