631b82709a97edcd54674a30f9b9ad01ad43938c
changvito <85725501+changvito@users.noreply.github.com>
2026-08-01 07:01:25 +0000

    Container With Most Water (cpp)

---
+++ 11-container-with-most-water/solution.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int Left = 0;
+        int Right = height.size() - 1;
+        int Max = 0;
+        while (Left < Right){
+            Max = max(Max, min(height[Left], height[Right]) * abs(Right - Left));
+            if (height[Left] <= height[Right]){
+                Left++;
+            }
+            else{
+                Right--;
+            }
+        }
+        return Max;
+    }
+};
