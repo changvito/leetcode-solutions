e650866dfe1b12d6b4225642d3521433f040d20e
changvito <85725501+changvito@users.noreply.github.com>
2026-08-01 09:22:38 +0000

    Best Time to Buy and Sell Stock (cpp)

--- 121-best-time-to-buy-and-sell-stock/solution.cpp
+++ 121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,16 +1,12 @@
 ﻿class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int MinPrice = prices[0];
+        int MinPrice = 1e9;
         int MaxProfit = 0;
 
-        for (int i = 0; i < prices.size(); i++){
-            if (prices[i] < MinPrice){
-                MinPrice = prices[i];
-            }
-            else if (prices[i] - MinPrice > MaxProfit){
-                MaxProfit = prices[i] - MinPrice;
-            }
+        for (int i : prices){
+            MinPrice = min(i, MinPrice);
+            MaxProfit = max(i-MinPrice, MaxProfit);
         }
         return MaxProfit;
     }
