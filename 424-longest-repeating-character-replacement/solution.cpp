180f3743e810a450ef94862aa3b1b822eabc0661
changvito <85725501+changvito@users.noreply.github.com>
2026-08-03 12:09:12 +0000

    Longest Repeating Character Replacement (cpp)

---
+++ 424-longest-repeating-character-replacement/solution.cpp
@@ -0,0 +1,19 @@
+﻿class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> Count(128, 0);
+        int Left = 0;
+        int MaxCount = 0;
+        int Longest = 0;
+        for (int Right = 0; Right < s.size(); Right++){
+            Count[s[Right]]++;
+            MaxCount = max(MaxCount, Count[s[Right]]);
+            if(Right - Left + 1 - MaxCount > k){
+                Count[s[Left]]--;
+                Left++;
+            }
+            Longest = max(Longest, Right - Left + 1);
+        }
+        return Longest;
+    }
+};
