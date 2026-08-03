6c5418506553088dbdafec0043d3bced1e716253
changvito <85725501+changvito@users.noreply.github.com>
2026-08-03 09:53:31 +0000

    Longest Substring Without Repeating Characters (cpp)

---
+++ 3-longest-substring-without-repeating-characters/solution.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> LastPos(128, -1);
+        int Left = 0;
+        int Max = 0;
+        for (int Right = 0; Right < s.size(); Right++){
+            if (LastPos[s[Right]] >= Left){
+                Left = LastPos[s[Right]] + 1;
+            }
+            LastPos[s[Right]] = Right;
+            Max = max(Max, Right - Left + 1);
+        }
+        return Max;
+    }
+};
