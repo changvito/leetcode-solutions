34b2b973cfc85a824b9f7103521240acd66098b0
changvito <85725501+changvito@users.noreply.github.com>
2026-08-18 12:27:56 +0000

    Minimum Window Substring (cpp)

---
+++ 76-minimum-window-substring/solution.cpp
@@ -0,0 +1,37 @@
+﻿class Solution {
+public:
+    string minWindow(string s, string t) {
+        if (s.empty() || t.empty() || s.length() == 0 || s.length() < t.length()){
+            return "";
+        }
+        vector<int> tWindow(128,0);
+        vector<int> sWindow(128,0);
+        for (int i = 0; i < t.size(); i++){
+            tWindow[t[i]]++;
+        }
+        int Count = t.length();
+        int Minimum = INT_MAX;
+        int MinLeft = 0;
+        int Left = 0;
+        int Right = 0;
+        while (Right < s.size()){
+            if(tWindow[s[Right]] > 0){
+                Count--;
+            }
+            tWindow[s[Right]]--;
+            Right++;
+            while (Count == 0){
+                if(Right - Left < Minimum){
+                    Minimum = Right - Left;
+                    MinLeft = Left;
+                }
+                tWindow[s[Left]]++;
+                if(tWindow[s[Left]] > 0){
+                    Count++;
+                }
+                Left++;
+            }
+        }
+        return Minimum == INT_MAX ? "" : s.substr(MinLeft, Minimum);
+    }
+};
