99f7c6a327c3c8d265a7afbf5bbccc7033a24faf
changvito <85725501+changvito@users.noreply.github.com>
2026-08-19 12:27:03 +0000

    Sliding Window Maximum (cpp)

---
+++ 239-sliding-window-maximum/solution.cpp
@@ -0,0 +1,21 @@
+﻿class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> Window;
+        vector<int> MaxArr;
+        int Max = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if (!Window.empty() && Window.front() < i - k + 1){
+                Window.pop_front();
+            }
+            while (!Window.empty() && nums[i] >= nums[Window.back()]){
+                Window.pop_back();
+            }
+            Window.push_back(i);
+            if(i >= k - 1){
+                MaxArr.push_back(nums[Window.front()]);
+            }
+        }
+        return MaxArr;
+    }
+};
