9db4cb7e0676a84d87c8cc2e62098d9184d12868
changvito <85725501+changvito@users.noreply.github.com>
2026-08-20 08:23:42 +0000

    Evaluate Reverse Polish Notation (cpp)

---
+++ 150-evaluate-reverse-polish-notation/solution.cpp
@@ -0,0 +1,40 @@
+﻿class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> RPN;
+        for (const string& c : tokens){
+            if (c == "+"){
+                int Second = RPN.top();
+                RPN.pop();
+                int First = RPN.top();
+                RPN.pop();
+                RPN.push(First + Second);
+            }
+            else if (c == "-"){
+                int Second = RPN.top();
+                RPN.pop();
+                int First = RPN.top();
+                RPN.pop();
+                RPN.push(First - Second);
+            }
+            else if (c == "*"){
+                int Second = RPN.top();
+                RPN.pop();
+                int First = RPN.top();
+                RPN.pop();
+                RPN.push(First * Second);
+            }
+            else if (c == "/"){
+                int Second = RPN.top();
+                RPN.pop();
+                int First = RPN.top();
+                RPN.pop();
+                RPN.push(First / Second);
+            }
+            else{
+                RPN.push(stoi(c));
+            }
+        }
+        return RPN.top();
+    }
+};
