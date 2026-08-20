21257f6d6fcf4525d4298ee10beac05ba3067730
changvito <85725501+changvito@users.noreply.github.com>
2026-08-20 03:44:19 +0000

    Valid Parentheses (cpp)

---
+++ 20-valid-parentheses/solution.cpp
@@ -0,0 +1,36 @@
+﻿class Solution {
+public:
+    bool isValid(string s) {
+        if (s.empty()){
+            return true;
+        }
+        stack<char> VP;
+        for (char c : s){
+            if (c == '(' || c =='[' || c == '{'){
+                VP.push(c);
+            }
+            else if ( c == ')'){
+                if (VP.empty() || VP.top() != '('){
+                    return false;
+                }
+                VP.pop();
+            }
+            else if ( c == ']'){
+                if (VP.empty() || VP.top() != '['){
+                    return false;
+                }
+                VP.pop();
+            }
+            else if ( c == '}'){
+                if (VP.empty() || VP.top() != '{'){
+                    return false;
+                }
+                VP.pop();
+            }
+        }
+        if (!VP.empty()){
+            return false;
+        }
+        return true;
+    }    
+};
