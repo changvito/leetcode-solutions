cbb5c560c6696d0b3b983542fc2df07294c01c3d
changvito <85725501+changvito@users.noreply.github.com>
2026-08-20 09:20:06 +0000

    Daily Temperatures (cpp)

---
+++ 739-daily-temperatures/solution.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> TempStack;
+        vector<int> Days(temperatures.size(),0);
+        for (int i = 0; i < temperatures.size(); i++){
+            while(!TempStack.empty() && temperatures[i] > temperatures[TempStack.top()]){
+                int Ind = TempStack.top();
+                TempStack.pop();
+                Days[Ind] = i -Ind;
+            }
+            TempStack.push(i);
+        }
+        return Days;
+    }
+};
