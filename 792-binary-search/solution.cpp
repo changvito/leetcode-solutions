d1a0e44c0c367460f713eeec4ec8391b23938397
changvito <85725501+changvito@users.noreply.github.com>
2026-08-22 10:06:55 +0000

    Binary Search (cpp)

---
+++ 792-binary-search/solution.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int Right = nums.size() - 1;
+        int Left = 0;
+        while (Left <= Right){
+            int Mid = Left + (Right - Left) / 2;
+            if (target == nums[Mid]){
+                return Mid;
+            }
+            else if (target < nums[Mid]){
+                Right = Mid - 1;
+            }
+            else {
+                Left = Mid + 1;
+            } 
+        }
+        return -1;
+    }
+};
