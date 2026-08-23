0cda604ea6d8762e2cecd53cd75b9f971fdff1ed
changvito <85725501+changvito@users.noreply.github.com>
2026-08-23 14:32:59 +0000

    Search a 2D Matrix (cpp)

--- 74-search-a-2d-matrix/solution.cpp
+++ 74-search-a-2d-matrix/solution.cpp
@@ -1,36 +1,20 @@
 ﻿class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int RowHead = 0;
-        int RowTail = matrix.size() - 1;
-        int ColumnHead = 0;
-        int ColumnTail = matrix[0].size() - 1;
-        while (RowHead <= RowTail){
-            int RowMid = RowHead + (RowTail - RowHead) / 2;
-            if (target == matrix[RowMid][0]){
+        int Head = 0;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int Tail = m * n - 1;
+        while (Head <= Tail){
+            int Mid = Head + (Tail - Head) / 2;
+            if (target == matrix[Mid / n][Mid % n]){
                 return true;
             }
-            else if (target < matrix[RowMid][0]){   
-               RowTail = RowMid - 1;
-            }
-            else {
-                RowHead = RowMid + 1;
-            }
-        }
-        if (RowTail < 0){
-            return false;
-        }
-        while (ColumnHead <= ColumnTail){
-
-            int ColumnMid = ColumnHead +(ColumnTail - ColumnHead) / 2;
-            if(target == matrix[RowTail][ColumnMid]){
-                return true;
-            }
-            else if (target < matrix[RowTail][ColumnMid]){
-                ColumnTail = ColumnMid - 1;
+            else if (target < matrix[Mid / n][Mid % n]){
+                Tail = Mid - 1;
             }
             else{
-                ColumnHead = ColumnMid + 1;
+                Head = Mid + 1;
             }
         }
         return false;
