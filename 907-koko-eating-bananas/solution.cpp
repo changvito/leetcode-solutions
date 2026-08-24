a1d617124409f90984f46560248243130cc294d4
changvito <85725501+changvito@users.noreply.github.com>
2026-08-24 07:05:59 +0000

    Koko Eating Bananas (cpp)

---
+++ 907-koko-eating-bananas/solution.cpp
@@ -0,0 +1,26 @@
+﻿class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int Left = 1;
+        int Right = 0;
+        for (int i = 0; i < piles.size(); i++){
+            Right = max(Right, piles[i]);
+        }
+        int k;
+        while (Left <= Right){
+            int Mid = Left + (Right - Left) / 2;
+            long long Hours = 0;
+            for (int i = 0; i < piles.size(); i++){
+                Hours += (piles[i] + Mid - 1) / Mid;
+            }
+            if (Hours <= h){
+                k = Mid;
+                Right = Mid - 1;
+            }
+            else if (Hours > h){
+                Left = Mid + 1;
+            }
+        }
+        return k;
+    }
+};
