c9459b251f6e05b614e11c8f2b4b6b559b36aebe
changvito <85725501+changvito@users.noreply.github.com>
2026-08-25 01:50:48 +0000

    Search in Rotated Sorted Array (cpp)

---
+++ 33-search-in-rotated-sorted-array/solution.cpp
@@ -0,0 +1,30 @@
+﻿class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int Left = 0;
+        int Right = nums.size() - 1;
+        while (Left <= Right){
+            int Mid = Left + (Right - Left) / 2;
+            if (target == nums[Mid]){
+                return Mid;
+            }
+            else if (nums[Mid] >= nums[Left]){
+                if (target >= nums[Left] && target < nums[Mid]){
+                    Right = Mid - 1;
+                }
+                else {
+                    Left = Mid + 1;
+                }
+            }
+            else {
+                if (target > nums[Mid] && target <= nums[Right]){
+                    Left = Mid + 1;
+                }
+                else {
+                    Right = Mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
