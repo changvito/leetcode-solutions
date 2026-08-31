11a761e80edd477872565049ddab5fb2979508cd
changvito <85725501+changvito@users.noreply.github.com>
2026-08-31 01:50:50 +0000

    Linked List Cycle (cpp)

---
+++ 141-linked-list-cycle/solution.cpp
@@ -0,0 +1,22 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set <ListNode*> Visited;
+        while (head != nullptr){
+            if (Visited.count(head)){
+                return true;
+            }
+            Visited.insert(head);
+            head = head -> next;
+        }
+        return false;
+    }
+};
