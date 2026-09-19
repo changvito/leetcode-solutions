558b4ebb68628520137bc851d41ed0fe381b67c8
changvito <85725501+changvito@users.noreply.github.com>
2026-09-19 01:54:57 +0000

    Copy List with Random Pointer (cpp)

---
+++ 138-copy-list-with-random-pointer/solution.cpp
@@ -0,0 +1,38 @@
+﻿/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (!head){
+            return nullptr;
+        }
+        unordered_map<Node*, Node*> mp;
+        Node* Cur = head;
+        while (Cur){
+            mp[Cur] = new Node(Cur -> val);
+            Cur = Cur -> next;
+        }
+
+        Cur = head;
+        while (Cur){
+            mp[Cur] -> next = mp[Cur -> next];
+            mp[Cur] -> random = mp[Cur -> random];
+            Cur = Cur ->  next; 
+        }
+        return mp[head];
+    }
+};
